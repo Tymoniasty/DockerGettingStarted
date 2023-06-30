f6529aa57ec9548f19542dd783c3a009d9b52e47
tymon <tymon@LenovoUbuntu.home>
2023-06-30 21:56:10 +0100

    label text change

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Item to the list!!'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
